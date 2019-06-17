e8b72a0db94c9521ea7f4b3419f088b994d8f833
gangeshk14 <51427112+gangeshk14@users.noreply.github.com>
2019-06-17 22:43:06 +0800

    Add files via upload

---
+++ weather/main.py
@@ -0,0 +1,58 @@
+import webapp2
+from google.appengine.api import urlfetch
+import json
+import jinja2
+import os
+import urllib
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
+
+    def post(self):
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        date = self.request.get("date")
+        time = self.request.get("time")
+        time_change = urllib.quote(time, safe='')
+        datetime = date+'T'+time_change+'%3A00'
+        location = self.request.get("location")
+        print(location)
+        print(time)
+        print(date)
+
+        endpointUrl = "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time="+datetime
+        #print(endpointUrl)
+
+        response = urlfetch.fetch(endpointUrl)
+
+        content = response.content
+
+        response_as_json = json.loads(content)
+        forecasts = response_as_json['items'][0]['forecasts']
+        #print(forecasts)
+        forecast = ''
+        for data in forecasts:
+            if data['area'] == location:
+                forecast = data['forecast']
+                break
+
+        info = {
+            "forecast":"Forecast at "+location+' on '+date+' '+time+' shows '+ forecast +' weather',
+            "location": location,
+            "time": time,
+            "date": date,
+        }
+        self.response.write(welcome_template.render(info))
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+
+], debug=True)
